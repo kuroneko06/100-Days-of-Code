af9169393d12c4acd2039fcef955ad225a48c9af
kuroneko06 <91165046+kuroneko06@users.noreply.github.com>
2021-09-24 20:57:30 +0700

    Create diamond.cpp

---
+++ Day 4/diamond.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int row;
+
+    cout << "=====Let's build a Pyramid====" << endl;
+    cout << "Input row of the Pyramid : ";
+    cin >> row;
+
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= row-i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+
+    for (int i = row; i >= 1; i--)
+    {
+        for (int j = 1; j <= row-i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << " *";
+        }
+        cout << endl;
+    }
+    
+}
